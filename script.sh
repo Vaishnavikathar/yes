c9c557308ff83bbcdaf6b17a7133412be0762d2d
Your Name <you@example.com>
2025-11-05 14:00:00 +0000

    Initial commit

---
+++ script.sh
@@ -0,0 +1,24 @@
+echo "Second change" >> app.py
+git add app.py
+git commit -m "Commit 2: second change"
+
+echo "Third change" >> app.py
+git add app.py
+git commit -m "Commit 3: third change"
+
+echo "Fourth change" >> app.py
+git add app.py
+git commit -m "Commit 4: fourth change"
+
+echo "Fifth change" >> app.py
+git add app.py
+git commit -m "Commit 5: fifth change"
+
+echo "Sixth change" >> app.py
+git add app.py
+git commit -m "Commit 6: sixth change"
+
+echo "Seventh change" >> app.py
+git add app.py
+git commit -m "Commit 7: seventh change"
+
